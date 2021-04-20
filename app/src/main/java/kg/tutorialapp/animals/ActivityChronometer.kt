9ebfebd670ad8567ee0f31a64cb9f699f080d254
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-04-21 01:19:36 +0600

    Done homework #26 Chronometer.

--- app/src/main/java/kg/tutorialapp/animals/ActivityChronometer.kt
+++ app/src/main/java/kg/tutorialapp/animals/ActivityChronometer.kt
@@ -2,13 +2,17 @@ package kg.tutorialapp.animals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Button
+import android.widget.Chronometer
 
 class ActivityChronometer : AppCompatActivity() {
 
     lateinit var btn_start: Button
     lateinit var btn_stop: Button
     lateinit var btn_reset: Button
+
+    private lateinit var chronometer: Chronometer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,19 @@ class ActivityChronometer : AppCompatActivity() {
         btn_start = findViewById(R.id.start)
         btn_stop = findViewById(R.id.stop)
         btn_reset = findViewById(R.id.reset)
+
+        chronometer = findViewById(R.id.chronometer)
+        chronometer.isCountDown = true
+
+        btn_start.setOnClickListener {
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.start()
+        }
+        btn_stop.setOnClickListener {
+            chronometer.stop()
+        }
+        btn_reset.setOnClickListener {
+            chronometer.base = SystemClock.elapsedRealtime()
+        }
     }
 }
